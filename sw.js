2c5a1f49fd356adb9fb436622db5bb97bc96f912
andreaweb <deiainluv@gmail.com>
2018-08-06 10:19:59 -0300

    sw with comments

--- sw.js
+++ sw.js
@@ -1,11 +1,16 @@
+//Assign number to our cache version
 let CURRENT_CACHES = {
 	offline: 'offline-v1'
 }
+
 const OFFLINE_URL = 'index.html';
 console.log("file added");
 
+//first event of service worker lifecycle
 self.addEventListener('install', event => {
   event.waitUntil(
+    //make a call to offline page while ensuring the url is unique through createCacheBustedRequest
+    //this way we can update our offline page with a new version later
     fetch(createCacheBustedRequest(OFFLINE_URL)).then(function(response) {
       return caches.open(CURRENT_CACHES.offline).then(function(cache) {
         return cache.put(OFFLINE_URL, response);
@@ -24,10 +29,13 @@ function createCacheBustedRequest(url) {
 }
 });
 
+//deletes oudated/unused caches because a sw can only be activated if all tabs of an earlier version are closed
 self.addEventListener('activate', event => {
   let expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key) {
     return CURRENT_CACHES[key];
   });
+  //confirms whether all required assets are already cached or not
+  //the service worker will only be installed when all files are cached
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
@@ -41,6 +49,7 @@ self.addEventListener('activate', event => {
   );
 });
 
+//fetch events after sw is installed and user navigates to different page or refreshes
 self.addEventListener('fetch', event => {
   if (event.request.mode === 'navigate' ||
       (event.request.method === 'GET' &&
